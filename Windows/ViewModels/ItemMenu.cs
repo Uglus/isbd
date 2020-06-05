8f7d618a0a6770ebcd61e5a8455378b7c9d101c3
Husein Janien <huseinjanien@gmail.com>
2020-06-05 19:48:13 +0300

    Second commit

---
+++ Windows/ViewModels/ItemMenu.cs
@@ -0,0 +1,33 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Windows.ViewModels
+{
+    public class ItemMenu
+    {
+        public ItemMenu(string header, List<SubItem> subItems, PackIconKind icon)
+        {
+            this.Header = header;
+            this.SubItems = subItems;
+            this.Icon = icon;
+        }
+        //public ItemMenu(string header, UserControl screen, PackIconKind icon)
+        //{
+        //    this.Header = header;
+        //    this.Screen = screen;
+        //    this.Icon = icon;
+        //}
+
+        public string Header { get; private set; }
+        public PackIconKind Icon { get; private set; }
+        public List<SubItem> SubItems { get; private set; }
+        public UserControl Screen { get; private set; }
+
+
+    }
+}
---
+++ Windows/Views/Create_Quiz.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Windows.Views
+{
+    /// <summary>
+    /// Interaction logic for Create_Quiz.xaml
+    /// </summary>
+    public partial class Create_Quiz : Window
+    {
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+
+        public Create_Quiz()
+        {
+            InitializeComponent();
+        }
+    }
+}
